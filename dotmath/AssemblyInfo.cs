928e308935adaec9a9b4ef5c2887d080b1856cf5
Aviv Keshet <akeshet@mit.edu>
2012-04-19 17:07:53 -0400

    Removed dotMath signing key.

--- dotmath/AssemblyInfo.cs
+++ dotmath/AssemblyInfo.cs
@@ -53,6 +53,4 @@ using System.Runtime.CompilerServices;
 //   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
 //       documentation for more information on this.
 //
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile(@"..\..\dotMath.snk")]
 //[assembly: AssemblyKeyName("")]
