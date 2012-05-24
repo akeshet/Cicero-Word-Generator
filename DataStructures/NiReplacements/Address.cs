cd8953ca3e6f20f82b497e247cf821bb1ae4f797
Aviv Keshet <akeshet@mit.edu>
2012-05-23 23:15:23 -0400

    Address made serializable.

--- DataStructures/NiReplacements/Address.cs
+++ DataStructures/NiReplacements/Address.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace DataStructures.Gpib
 {
+    [Serializable, TypeConverter(typeof(ExpandableStructConverter))]
     public struct Address
     {
 
