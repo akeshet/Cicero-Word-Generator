43d3aa995403b31a0bd3b459e5f634cb142136b0
Aviv Keshet <akeshet@gmail.com>
2012-06-19 14:59:50 -0400

    Add NetworkDatagram and associated unit test

---
+++ DataStructures/Timing/NetworkClockDatagram.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructures.Timing
+{
+    public class NetworkClockDatagram
+    {
+        private UInt32 elaspedTime;
+
+        public UInt32 ElaspedTime
+        {
+            get { return elaspedTime; }
+        }
+        private UInt32 clockID;
+
+        public UInt32 ClockID
+        {
+            get { return clockID; }
+        }
+
+        public NetworkClockDatagram(UInt32 elaspedTime, UInt32 clockID)
+        {
+            this.elaspedTime = elaspedTime;
+            this.clockID = clockID;
+        }
+
+        public NetworkClockDatagram(byte[] byteStream, int offset)
+        {
+            if (byteStream.Length - offset < 8)
+                throw new SoftwareClockProvider.SoftwareClockProviderException ("Attempted to construct a datagram from too short bytestream");
+
+            elaspedTime = BitConverter.ToUInt32(byteStream, offset);
+            clockID = BitConverter.ToUInt32(byteStream, offset + 4);
+        }
+
+        public NetworkClockDatagram(byte[] byteStream) : this(byteStream, 0) { }
+
+        public byte[] toByteStream()
+        {
+            byte[] ans = new byte[8];
+            byte[] etBytes = BitConverter.GetBytes(elaspedTime);
+            byte[] clByes = BitConverter.GetBytes(clockID);
+
+            for (int i = 0; i < 4; i++)
+            {
+                ans[i] = etBytes[i];
+                ans[i + 4] = clByes[i];
+            }
+
+            return ans;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is NetworkClockDatagram))
+                return false;
+            NetworkClockDatagram other = (NetworkClockDatagram)obj;
+            if (other.clockID != clockID)
+                return false;
+            if (other.elaspedTime != elaspedTime)
+                return false;
+            return true;
+        }
+
+    }
+}
---
+++ DataStructures/Timing/NetworkClockProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace DataStructures.Timing
+{
+    public class NetworkClockProvider : SoftwareClockProvider
+    {
+        private static Thread listenerThread;
+
+        public static readonly int networkClockDestinationPort = 39721;
+
+        public static void startListener() {
+            if (listenerThread != null)
+            {
+                throw new SoftwareClockProviderException("Attempted to start listener thread when already started.");
+            }
+            listenerThread = new Thread(listenerThreadProc);
+            listenerThread.Start();
+        }
+
+        public static void shutDown()
+        {
+            if (listenerThread == null)
+            {
+                throw new SoftwareClockProviderException("Attempted to stop a listener that was not running.");
+            }
+            listenerThread.Abort();
+            listenerThread = null;
+        }
+
+        public static bool listenerRunning()
+        {
+            return (listenerThread != null);
+        }
+
+        private static UdpClient udpClient;
+
+        private static void listenerThreadProc()
+        {
+            udpClient = new UdpClient(networkClockDestinationPort);
+        }
+
+        protected override void abortTimer()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void armTimer()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void startTimer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
