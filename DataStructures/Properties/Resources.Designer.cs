726a8ef573ca73cc74657b35cf75847e14277f09
Aviv Keshet <akeshet@gmail.com>
2012-06-25 13:54:00 -0400

    Version 1.63 beta.

--- DataStructures/Properties/Resources.Designer.cs
+++ DataStructures/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace DataStructures.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to unreleased.
+        ///   Looks up a localized string similar to Beta.
         /// </summary>
         internal static string RevisionString {
             get {
