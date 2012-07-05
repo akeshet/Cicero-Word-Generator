1876d138a2dd3ff49220f8ca10a90c6b388e9ff6
Aviv Keshet <akeshet@gmail.com>
2012-07-05 19:35:51 -0400

    Even more realistic test

--- WordGenerator/Controls/MainClientForm.cs
+++ WordGenerator/Controls/MainClientForm.cs
@@ -853,7 +853,7 @@ namespace WordGenerator
         private void inspectVariableTimebaseToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Storage.sequenceData.createLoopCopies();
-            TimestepTimebaseSegmentCollection ans = Storage.sequenceData.generateVariableTimebaseSegments(SequenceData.VariableTimebaseTypes.AnalogGroupControlledVariableFrequencyClock, .0000001);
+            TimestepTimebaseSegmentCollection ans = Storage.sequenceData.generateVariableTimebaseSegments(SequenceData.VariableTimebaseTypes.AnalogGroupControlledVariableFrequencyClock, (1.0 / (double)10000000));
             Storage.sequenceData.cleanupLoopCopies();
 
             PropertyGridForm dialog = new PropertyGridForm(ans);
