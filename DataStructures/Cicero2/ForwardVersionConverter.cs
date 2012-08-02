40d6ba6e38cc74dcd2293f2eef35658145b2e582
Aviv Keshet <akeshet@gmail.com>
2012-08-02 17:27:42 -0400

    Non-working commit, for merging purposes.

---
+++ DataStructures/Cicero2/ForwardVersionConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cicero.DataStructures2
+{
+    public static class ForwardVersionConverter
+    {
+        public static ResourceID<Waveform> convert(DataStructures.Waveform input, Cicero2ResourceDictionary resources)
+        {
+
+        }
+    }
+}
