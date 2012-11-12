1e8743477e1170219286f20f8ac8356cc077abc6
Timur Rvachov <timur11@gmail.com>
2012-11-12 15:00:54 -0500

    Initial commit of ver 1.65. Changed ver name.

--- DataStructures/Properties/Resources.Designer.cs
+++ DataStructures/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace DataStructures.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to rev6.
+        ///   Looks up a localized string similar to .
         /// </summary>
         internal static string RevisionString {
             get {
@@ -97,7 +97,7 @@ namespace DataStructures.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.64.
+        ///   Looks up a localized string similar to 1.65.
         /// </summary>
         internal static string VersionString {
             get {
