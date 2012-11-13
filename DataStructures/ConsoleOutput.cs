733913c79eadde96779d78b7c3f60cebce55839d
Timur Rvachov <timur11@gmail.com>
2012-11-12 22:02:07 -0500

    Added DEBUG on to see NetworkClock communication in log.

---
+++ DataStructures/ConsoleOutput.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles; 
+
+namespace DataStructures
+{
+    public class ConsoleOutput
+    {
+        public ConsoleOutput()
+        {
+            AllocConsole();
+            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            Encoding encoding = System.Text.Encoding.GetEncoding(MY_CODE_PAGE);
+            StreamWriter standardOutput = new StreamWriter(fileStream, encoding);
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+        }
+
+
+
+        [DllImport("kernel32.dll",
+           EntryPoint = "GetStdHandle",
+           SetLastError = true,
+           CharSet = CharSet.Auto,
+           CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const int MY_CODE_PAGE = 437;
+
+       
+        public void WriteOut(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+    }
+}
--- DataStructures/Timing/NetworkClockProvider.cs
+++ DataStructures/Timing/NetworkClockProvider.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿#define DEBUG
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Net.Sockets;
+
 
 namespace DataStructures.Timing
 {
