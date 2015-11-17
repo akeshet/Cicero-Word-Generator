0239c52f97b6384f56e282c05fd87f8e0e48b27e
Timur Rvachov <timur11@gmail.com>
2015-11-16 19:28:35 -0500

    Commented the new code a bit better.

--- DataStructures/SettingsData/ChannelCollection.cs
+++ DataStructures/SettingsData/ChannelCollection.cs
@@ -24,14 +24,28 @@ namespace DataStructures
 
         public void MoveValue(int key, int target)
         {
-            //hold on to logical channels that will be swapped
-            LogicalChannel lc1 = channels[key];
-            //LogicalChannel lc2 = channels[target];
+            //I'm not sure how this functions should behave: the channels object is a dictionary, which is
+            //unordered, and in principle it is such that the key is somehow related to the value. Yet the
+            //channels are drawn in an order by their keys...
 
+            //So here is one weird thing: if I remove a channel, I will also remove that key. So there could
+            //be holes in the keys, e.g. 1,2,4,5 (3 is missing). But this MoveValue function will fill in the
+            //holes, because it will dump the channels dictionary to a list, and then repopulate the dictionary
+            //with the proper LogicalChannel moved. The repopulation of the dictionary uses sequential keys, ie
+            //1,2,3 etc. So if there was a hole in the keys, it will be filled. Is this important? Does anyway
+            //purposely want holes floating around? I dont know.
+            
+            //hold on to logical channel that will be inserted
+            LogicalChannel lc1 = channels[key];
+           
+            //remove the logical channel that we will late re-insert
             RemoveChannel(key);
 
+
+            //dump dictionary to a list (only possible because of System.Linq, I think)
             List <KeyValuePair<int, LogicalChannel>> tempList = channels.ToList();
 
+            //sort the list by keys to preserve old ordering when repopulating
             tempList.Sort(
                 delegate (KeyValuePair<int, LogicalChannel> p1, KeyValuePair<int,LogicalChannel> p2)
                 {
@@ -40,6 +54,8 @@ namespace DataStructures
 
             int newKey=0;
             channels.Clear();
+
+            //repopulate list, inserting the moved logical channel when we are at the right key value
             foreach(KeyValuePair<int,LogicalChannel> i in tempList)
             {
                 if (newKey == target)
