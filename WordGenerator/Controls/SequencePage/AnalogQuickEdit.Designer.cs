0fbbd1b1e2e36ee597b3c2f070820b8dd9eed50f
Samarth Vadia <samarth.vadia12@gmail.com>
2016-06-07 19:29:39 -0400

    Analog Swap Edition
    Implemented by Will. Using AnalogQuickEdit from SequencePage (similar to
    digital swap done by QuickEdit)

---
+++ WordGenerator/Controls/SequencePage/AnalogQuickEdit.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WordGenerator.Controls
+{
+    partial class AnalogQuickEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.IDText = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose a new ID for this\r\nanalog channel";
+            // 
+            // IDText
+            // 
+            this.IDText.Location = new System.Drawing.Point(15, 50);
+            this.IDText.Name = "IDText";
+            this.IDText.Size = new System.Drawing.Size(100, 20);
+            this.IDText.TabIndex = 4;
+            this.IDText.TextChanged += new System.EventHandler(this.IDText_TextChanged_1);
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(15, 85);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(124, 23);
+            this.SubmitButton.TabIndex = 3;
+            this.SubmitButton.Text = "Apply";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // AnalogQuickEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(152, 124);
+            this.Controls.Add(this.IDText);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.label1);
+            this.Name = "AnalogQuickEdit";
+            this.Text = "AnalogQuickEdit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IDText;
+        private System.Windows.Forms.Button SubmitButton;
+    }
+}
---
+++ WordGenerator/Controls/SequencePage/AnalogQuickEdit.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DataStructures;
+
+namespace WordGenerator.Controls
+{
+    public partial class AnalogQuickEdit : Form
+    {
+        private int incomingID;
+
+        public AnalogQuickEdit(int selectedID)
+        {
+            InitializeComponent();
+            IDText.Text = selectedID.ToString();
+            incomingID = selectedID;
+
+        }
+
+
+
+        private void IDText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            int newKey;
+            if (int.TryParse(IDText.Text, out newKey))
+            {
+                //Set up the datastructures
+                ChannelCollection selectedChannelCollection;
+                HardwareChannel.HardwareConstants.ChannelTypes selectedChannelType;
+
+
+
+                //Bring in all the data, and call the new SwapChannels function
+                string selectedTypeString = "analog";
+                selectedChannelType = HardwareChannel.HardwareConstants.ParseChannelTypeFromString(selectedTypeString);
+                selectedChannelCollection = Storage.settingsData.logicalChannelManager.GetDeviceCollection(selectedChannelType);
+
+                //need to reshuffle the digital data and pulse data in the timesteps....
+                AnalogGroupChannelData tempDic = new AnalogGroupChannelData();
+                int numberOfSteps = Storage.sequenceData.TimeSteps.Count();
+                int numberOfSwaps = newKey - incomingID; //The sign of this int tells us which way we "climb" through the dictionary
+                                                         //Simple case of an unoccupied destination key:
+                if (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(newKey))
+                {
+                    for (int w = 0; w < numberOfSteps; w++)
+                    {
+                        if (Storage.sequenceData.TimeSteps[w].AnalogGroup != null) //  "Continue" shows up as a "null" analog group
+                        {
+                            Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[newKey] = Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[incomingID];
+                            Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[incomingID] = new AnalogGroupChannelData();
+                        }
+                    }
+                    selectedChannelCollection.SwapChannels(incomingID, newKey);
+                }
+                else
+                {
+                    //Complicated case of an occupied destination key:
+                    for (int w = 0; w < numberOfSteps; w++)
+                    {
+                        #region Pairwise swaps to move the digital data along
+                        if (numberOfSwaps > 0)
+                        {
+                            for (int j = incomingID; j < newKey; j++)
+                            {
+                                while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(j)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+                                { j++; }
+
+                                if (j >= newKey)
+                                {
+                                    break;
+                                }
+
+                                int nextJ = j + 1;
+                                while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(nextJ)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+                                { nextJ++; }
+
+                                if (Storage.sequenceData.TimeSteps[w].AnalogGroup != null) //  "Continue" shows up as a "null" analog group
+                                {
+                                    tempDic = Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[j];
+                                }
+
+                                #region Find if a channel already has the next desired ID
+                                int conflictFlag = 0;
+                                Dictionary<int, LogicalChannel>.KeyCollection keys = Storage.settingsData.logicalChannelManager.ChannelCollections[DataStructures.HardwareChannel.HardwareConstants.ChannelTypes.analog].Channels.Keys;
+                                foreach (int i in keys)
+                                    if (i == newKey)
+                                        conflictFlag = 1;
+                                #endregion
+                                //If so, we need to delete THAT channel and re-add it with the old ID
+                                if (conflictFlag == 1)
+                                {
+                                    if (Storage.sequenceData.TimeSteps[w].AnalogGroup != null) //  "Continue" shows up as a "null" analog group
+                                    {
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[j] = Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[nextJ];
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[nextJ] = tempDic;
+                                    }
+
+                                }
+                                //If not, we just add the new channel at the new ID   
+                                else
+                                {
+                                    if (Storage.sequenceData.TimeSteps[w].AnalogGroup != null) //  "Continue" shows up as a "null" analog group
+                                    {
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[j] = new AnalogGroupChannelData();
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[nextJ] = tempDic;
+                                    }
+                                }
+
+
+                            }
+                        }
+                        else if (numberOfSwaps < 0)
+                        {
+                            for (int j = incomingID; j > newKey; j--)
+                            {
+                                while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(j)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+                                { j--; }
+
+                                if (j <= newKey)
+                                {
+                                    break;
+                                }
+
+                                int nextJ = j - 1;
+                                while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(nextJ)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+
+                                { nextJ--; }
+                                if (Storage.sequenceData.TimeSteps[w].AnalogGroup != null) //  "Continue" shows up as a "null" analog group
+                                {
+                                    tempDic = Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[j];
+                                }
+
+                                #region Find if a channel already has the next desired ID
+                                int conflictFlag = 0;
+                                Dictionary<int, LogicalChannel>.KeyCollection keys = Storage.settingsData.logicalChannelManager.ChannelCollections[DataStructures.HardwareChannel.HardwareConstants.ChannelTypes.analog].Channels.Keys;
+                                foreach (int i in keys)
+                                    if (i == newKey)
+                                        conflictFlag = 1;
+                                #endregion
+                                //If so, we need to delete THAT channel and re-add it with the old ID
+                                if (conflictFlag == 1)
+                                {
+                                    if (Storage.sequenceData.TimeSteps[w].AnalogGroup != null) //  "Continue" shows up as a "null" analog group
+                                    {
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[j] = Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[nextJ];
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[nextJ] = tempDic;
+                                    }
+
+                                }
+                                //If not, we just add the new channel at the new ID   
+                                else
+                                {
+                                    if (Storage.sequenceData.TimeSteps[w].AnalogGroup != null) //  "Continue" shows up as a "null" analog group
+                                    {
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[j] = new AnalogGroupChannelData();
+                                        Storage.sequenceData.TimeSteps[w].AnalogGroup.ChannelDatas[nextJ] = tempDic;
+                                    }
+                                }
+
+
+                            }
+
+                        }
+                        #endregion
+                    }
+                    #region Pairwise swaps to move the analog settings
+                    if (numberOfSwaps > 0)
+                    {
+                        for (int j = incomingID; j < newKey; j++)
+                        {
+                            while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(j)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+                            { j++; }
+
+                            if (j >= newKey)
+                            {
+                                break;
+                            }
+
+
+                            int nextJ = j + 1;
+                            while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(nextJ)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+                            { nextJ++; }
+                            selectedChannelCollection.SwapChannels(j, nextJ);
+                        }
+                    }
+                    else if (numberOfSwaps < 0)
+                    {
+                        for (int j = incomingID; j > newKey; j--)
+                        {
+                            while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(j)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+                            { j--; }
+                            if (j <= newKey)
+                            {
+                                break;
+                            }
+                            int nextJ = j - 1;
+                            while (!Storage.settingsData.logicalChannelManager.Analogs.ContainsKey(nextJ)) //Sometimes there are gaps in the key sequence because users can choose ANY ID
+                            { nextJ--; }
+                            selectedChannelCollection.SwapChannels(j, nextJ);
+                        }
+
+                    }
+                    #endregion
+                }
+
+
+
+                //Kill this form
+                this.Close();
+            }
+        }
+
+        private void IDText_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
