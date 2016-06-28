53e8a05c1f038e857ce392acdc0760b9194ef64f
BobbyDavid <lundenwill@gmail.com>
2016-06-28 13:57:42 -0400

    Adding Latest BEC 5 Database Helper Solution and Binaries

---
+++ DatabaseHelper3/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "write1VarEntry";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "write2VarEntry";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(107, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 117);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ DatabaseHelper3/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DatabaseHelper;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Int16[,] testIm = new Int16[1000,1000];
+        BinaryFormatter bf = new BinaryFormatter();
+        MemoryStream stream = new MemoryStream();
+        System.Diagnostics.Stopwatch st1 = new System.Diagnostics.Stopwatch();
+      DatabaseHelper.DatabaseHelper dbHelper = new DatabaseHelper.DatabaseHelper("192.168.1.227","192.168.1.227","root","w0lfg4ng","BECVDatabase"); //make a DB connection
+      
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          //  imageStruct f = dbHelper.readImage();
+          //  variableStruct df = dbHelper.readVariableValues();
+          //  f = dbHelper.readImage();
+           // df = dbHelper.readVariableValues();
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            dbHelper.readVariableValues();
+           
+
+
+
+           // ------ Code for exporting images for use in Julia -----
+            /* 
+            using (FileStream fs = new FileStream("atomData.dat", FileMode.CreateNew))
+            {
+                using (BinaryWriter w = new BinaryWriter(fs))
+                {
+                    for (int i = 0; i < bytesAtoms.Length; i++)
+                    {
+                        w.Write(bytesAtoms[i]);
+                    }
+                }
+            }
+            using (FileStream fs = new FileStream("noAtomData.dat", FileMode.CreateNew))
+            {
+                using (BinaryWriter w = new BinaryWriter(fs))
+                {
+                    for (int i = 0; i < bytesNoAtoms.Length; i++)
+                    {
+                        w.Write(bytesNoAtoms[i]);
+                    }
+                }
+            }
+            using (FileStream fs = new FileStream("darkData.dat", FileMode.CreateNew))
+            {
+                using (BinaryWriter w = new BinaryWriter(fs))
+                {
+                    for (int i = 0; i < bytesDark.Length; i++)
+                    {
+                        w.Write(bytesDark[i]);
+                    }
+                }
+            }
+             */
+            //---------
+
+
+            
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // dbHelper.readVariableValues(2);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           // dbHelper.readVariableValues(3);
+        }
+    }
+
+}
